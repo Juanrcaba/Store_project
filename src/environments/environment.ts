3f4629dbe5da504f7c51771735bb7f2c0c639ef3
juanrcaba <juanrscaba@gmail.com>
2022-07-03 17:11:06 -0400

    FirstCommit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'controlalmacen-425d7',
+    appId: '1:623226325786:web:642698fedac498762f7aa3',
+    storageBucket: 'controlalmacen-425d7.appspot.com',
+    apiKey: 'AIzaSyDJD5Vp7-tOI-ZiR78NCHulhMcVEu2z3Rg',
+    authDomain: 'controlalmacen-425d7.firebaseapp.com',
+    messagingSenderId: '623226325786',
+  },
   production: false
 };
 
